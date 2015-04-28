78f99a5cd8e04e8572a5907f3225d1a1f74738da
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-29 02:31:21 +0300

    update files

--- app/src/main/java/com/stenbergroom/bottomsheet/app/Main.java
+++ app/src/main/java/com/stenbergroom/bottomsheet/app/Main.java
@@ -1,21 +1,39 @@
 package com.stenbergroom.bottomsheet.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import com.cocosw.query.CocoQuery;
 
 
 public class Main extends Activity implements AdapterView.OnItemClickListener {
 
+    private ArrayAdapter<String> adapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ui_main);
+        CocoQuery q = new CocoQuery(this);
+
+        String[] items = new String[]{"From Xml", "Without Icon", "Dark Theme", "Grid", "Style", "Style from Theme", "ShareAction", "FullScreen","Menu Manipulate"};
+        q.id(R.id.listView)
+                .adapter(adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, items))
+                .itemClicked(this);
     }
+
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+        Intent intent = new Intent(this, ListActivity.class).setFlags(position).putExtra("title", adapter.getItem(position));
+        if (position == 5) {
+            intent.putExtra("style", true);
+        }
+        startActivity(intent);
     }
+
 }
