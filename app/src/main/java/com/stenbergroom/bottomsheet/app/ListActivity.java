461dbab7040f0ebe1400472e537e8770e5b96b04
Aleksandr Marchenko <marrchenko@gmail.com>
2015-04-28 20:07:03 +0300

    update files

---
+++ app/src/main/java/com/stenbergroom/bottomsheet/app/ListActivity.java
@@ -0,0 +1,18 @@
+package com.stenbergroom.bottomsheet.app;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.AdapterView;
+
+/**
+ * Created by Sten on 28.04.2015.
+ */
+public class ListActivity extends Activity implements AdapterView.OnItemClickListener {
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
---
+++ app/src/main/java/com/stenbergroom/bottomsheet/app/Main.java
@@ -0,0 +1,23 @@
+package com.stenbergroom.bottomsheet.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+public class Main extends Activity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+}
